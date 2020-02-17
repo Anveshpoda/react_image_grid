6da30c5cdba5f1b4a93c0caa2f688205a1d79b58
Anvesh Poda <anvesh@getwalk.in>
2020-02-17 13:40:23 +0530

    added some images

--- src/data/data.js
+++ src/data/data.js
@@ -54,18 +54,29 @@ export const images = [{
     name:"Anvesh Annu",
     url:require('../images/img14.jpeg'),
     des:"“Watch your thoughts; they become words. Watch your words; they become actions. Watch your actions; they become habits. Watch your habits; they become character. Watch your character; it becomes your destiny.”— Lao-Tze"
+},{
+    name:"Crzz Hittler",
+    url:require('../images/img15.jpeg'),
+    des:" “Sing like no one’s listening, love like you’ve never been hurt, dance like nobody’s watching, and live like it’s heaven on earth.” – (Attributed to various sources)"
+},{
+    name:"Neel Arem22",
+    url:require('../images/img16.jpeg'),
+    des:" “Curiosity about life in all of its aspects, I think, is still the secret of great creative people.” – Leo Burnett"
+},{
+    name:"Image World",
+    url:require('../images/img17.jpeg'),
+    des:"“Life is not a problem to be solved, but a reality to be experienced.”– Soren Kierkegaard"
+},{
+    name:"Ark Tech",
+    url:require('../images/img18.jpeg'),
+    des:" “Everybody wants to be famous, but nobody wants to do the work. I live by that. You grind hard so you can play hard. At the end of the day, you put all the work in, and eventually it’ll pay off. It could be in a year, it could be in 30 years. Eventually, your hard work will pay off.” — Kevin Hart"
+},{
+    name:"Real World",
+    url:require('../images/img19.jpeg'),
+    des:"“Life imposes things on you that you can’t control, but you still have the choice of how you’re going to live through this.” — Celine Dion"
+},{
+    name:"Anvesh Annu",
+    url:require('../images/img20.jpeg'),
+    des:"“Watch your thoughts; they become words. Watch your words; they become actions. Watch your actions; they become habits. Watch your habits; they become character. Watch your character; it becomes your destiny.”— Lao-Tze"
 },
-// {
-//     name:"",
-//     url:require('images/img15.jpeg'),
-//     des:""
-// },{
-//     name:"",
-//     url:"./images/img16",
-//     des:""
-// },{
-//     name:"",
-//     url:"./images/img17",
-//     des:""
-// }
 ]
